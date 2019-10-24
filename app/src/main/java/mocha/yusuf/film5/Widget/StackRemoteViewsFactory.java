42ff2cc380da62495b6b287349deb20a1c361833
Mochamad Yusuf <mochyusuf100@gmail.com>
2019-10-24 09:57:23 +0700

    Widget

---
+++ app/src/main/java/mocha/yusuf/film5/Widget/StackRemoteViewsFactory.java
@@ -0,0 +1,78 @@
+package mocha.yusuf.film5.Widget;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mocha.yusuf.film5.R;
+
+public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    private final List<Bitmap> mWidgetItems = new ArrayList<>();
+    private final Context mContext;
+
+    StackRemoteViewsFactory(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate() {
+    }
+
+    @Override
+    public void onDataSetChanged() {
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.getResources(), R.drawable.poster_t34));
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.getResources(), R.drawable.tv_the_walking_dead));
+    }
+
+    @Override
+    public void onDestroy() {
+    }
+
+    @Override
+    public int getCount() {
+        return mWidgetItems.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_item);
+        rv.setImageViewBitmap(R.id.imageView, mWidgetItems.get(position));
+
+        Bundle extras = new Bundle();
+        extras.putInt(ImagesBannerWidget.EXTRA_ITEM, position);
+        Intent fillInIntent = new Intent();
+        fillInIntent.putExtras(extras);
+
+        rv.setOnClickFillInIntent(R.id.imageView, fillInIntent);
+        return rv;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+}
---
+++ app/src/main/java/mocha/yusuf/film5/Widget/StackWidgetService.java
@@ -0,0 +1,11 @@
+package mocha.yusuf.film5.Widget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class StackWidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new StackRemoteViewsFactory(this.getApplicationContext());
+    }
+}
